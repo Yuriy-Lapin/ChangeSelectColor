c76db9174ffae0ef3cd038b5890aba9e81b72865
Yuriy-Lapin <yulapin2020@itcollege.lviv.ua>
2023-05-19 21:33:19 +0300

    new tests

--- test_main.py
+++ test_main.py
@@ -1,7 +1,17 @@
-import main
+from main import hello
 
-def test_main():
-    if main.hello() == 'Hello from Python':
-        print('Test ... OK')
-    else:
-        print('Test ... FAIL')
+
+def test_hello1():
+    assert hello() == 'Hello from Python'
+
+
+def test_hello2():
+    assert hello().split()[0] == 'Hello'
+
+
+def test_hello3():
+    assert hello().split()[1] == 'from'
+
+
+def test_hello4():
+    assert hello().split()[2] == 'Python'
